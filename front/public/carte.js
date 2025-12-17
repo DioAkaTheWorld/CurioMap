498f9be95d1d7891df9df1ac8ee1f0da5a12e1ab
Quent1540 <dieudonnequent1@gmail.com>
2025-12-17 10:59:06 +0100

    localisation de l'utilisateur sur la map(pas tres precise sur PC)

--- front/public/carte.js
+++ front/public/carte.js
@@ -1,5 +1,5 @@
-//Pour le moment on centre juste la carte sur Paris : 48.8566, 2.3522 avec un zoom de 13
-var map = L.map('map').setView([48.8566, 2.3522], 13);
+//Par défaut on centre la carte sur Paris, avec un zoom de 13
+const map = L.map('map').setView([48.8566, 2.3522], 13);
 
 //Ajout des tuiles (=le fond de carte visuel de la map)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,6 +7,22 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+//Si on peut localiser l'utilisateur, on récupère sa position et on centre la carte dessus
+if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        map.setView([lat, lon], 13);
+
+        //Ajouter un marqueur à la position de l'utilisateur
+        const userMarker = L.marker([lat, lon]).addTo(map);
+        userMarker.bindPopup("<b>Vous êtes ici !</b>").openPopup();
+
+    }, () => {
+        console.log("La géolocalisation a échoué");
+    });
+}
+
 //Ex d'ajout de marqueur manuellement pour voir si ça marche
-var marker = L.marker([48.8566, 2.3522]).addTo(map);
+const marker = L.marker([48.8566, 2.3522]).addTo(map);
 marker.bindPopup("<b>Salut !</b><br>Exemple point d'intérêt").openPopup();

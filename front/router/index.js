8de13160a857303ce6a812ffb75fbdfc60a2a482
Quent1540 <dieudonnequent1@gmail.com>
2026-02-03 10:56:43 +0100

    debut de l'agenda

--- front/router/index.js
+++ front/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Map from '../views/Map.vue'
+import Agenda from '../views/Agenda.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
             name:'Map',
             component: Map
         },
+        {
+            path:'/agenda',
+            name:'Agenda',
+            component: Agenda
+        }
     ],
 })
 
---
+++ front/views/Agenda.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="calendar-wrapper">
+    <ScheduleXCalendar :calendar-app="calendarApp" />
+  </div>
+</template>
+
+<script setup>
+import { ScheduleXCalendar } from "@schedule-x/vue"
+import {
+  createCalendar,
+  createViewMonthGrid,
+  createViewWeek,
+  createViewDay
+} from "@schedule-x/calendar"
+import '@schedule-x/theme-default/dist/index.css'
+
+const calendarApp = createCalendar({
+  views: [
+    createViewMonthGrid(),
+    createViewWeek(),
+    createViewDay(),
+  ],
+  selectedDate: Temporal.PlainDate.from('2026-02-02'),
+  locale: 'fr-FR',
+  events: [
+    {
+      id: 1,
+      title: 'Test Evenement',
+      start: Temporal.PlainDateTime.from('2026-02-02T10:00').toZonedDateTime('Europe/Paris'),
+      end: Temporal.PlainDateTime.from('2026-02-02T12:00').toZonedDateTime('Europe/Paris'),
+    }
+  ]
+})
+</script>
+
+<style scoped>
+.calendar-wrapper {
+  height: 800px;
+  width: 100%;
+}
+</style>

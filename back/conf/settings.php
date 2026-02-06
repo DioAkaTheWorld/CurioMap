982d618e54e19715e4e324cba7efe5b04f54e1de
Mattéo <matteo.cadet9@etu.univ-lorraine.fr>
2026-02-06 11:26:22 +0100

    tjr webetu mais jpp donc je stop

--- back/conf/settings.php
+++ back/conf/settings.php
@@ -2,9 +2,9 @@
 return [
     'settings' => [
         'db' => [
-            'dsn' => 'pgsql:host=curiomap.db;port=5432;dbname=curioMap',
-            'user' => 'admin',
-            'password' => 'admin',
+            'dsn' => 'pgsql:host=' . ($_ENV['POSTGRES_HOST'] ?? 'localhost') . ';port=5432;dbname=' . ($_ENV['POSTGRES_DB'] ?? 'curioMap'),
+            'user' => $_ENV['POSTGRES_USER'] ?? 'admin',
+            'password' => $_ENV['POSTGRES_PASSWORD'] ?? 'admin',
         ],
         'jwt' => [
             'key' => 'clef',
